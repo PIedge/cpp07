80fdcc4e1c50dcd14311f24a887b4e2677ee3ae4
tmerrien <tmerrien@studemt.42.fr>
2022-03-15 00:17:53 +0100

    added securities to ex01

--- ex01/Iter.hpp
+++ ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: tmerrien <tmerrien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 17:57:26 by tmerrien          #+#    #+#             */
-/*   Updated: 2022/03/13 20:21:16 by tmerrien         ###   ########.fr       */
+/*   Updated: 2022/03/15 00:17:14 by tmerrien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 # define ITER_HPP
 
 template <typename T>
-void iter(T *tab, int size, void (*func)(T const &))
+void iter(T *tab, unsigned int size, void (*func)(T const &))
 {
-	int i;
+	unsigned int i;
 
 	i = 0;
-	if (size <= 0)
+	if (size <= 0 || !tab || !func)
 		return;
 	while (i < size)
 		func(tab[i++]);
 }
 
 // template <typename T>
-// void iter(T *tab, int size, void (*func)(T &))
+// void iter(T *tab, unsigned int size, void (*func)(T &))
 // {
-// 	int i;
+// 	unsigned int i;
 
 // 	i = 0;
-// 	if (size <= 0)
+// 	if (size <= 0 || !tab || !func)
 // 		return;
 // 	while (i < size)
 // 		func(tab[i++]);
